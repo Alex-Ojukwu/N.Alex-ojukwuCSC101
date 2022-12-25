5a71fb08281219339e361da08077796319c1e186
nduka <nduka.alex-ojukwu@pau.edu.ng>
2022-12-25 17:51:45 +0100

    (my third commit)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    //addition
+    let sum = 5550 + 7310;
+    println! ("the sum of 5550 and 7310 = {}",sum);
+
+    //subtraction
+    let difference:f64 = 95.5 - 4.3;
+    println!("the difference of 95.5 and 4.3 = {}", difference);
+
+    //multiplication
+    let product:f64= 4.0 * 30.0;
+    println! ("the product of 4 and 30 is{}",product);
+
+    //division
+    let division:f64 = 56.7 / 32.2;
+    println! ("the division of 56.7 and 32.2 ={}",division);
+
+    //remainder
+    let remainder:f64 = 43.0 % 5.0;
+    println! ("the remainder  of 43 and 5 ={}",remainder)
+
+}
---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\NDUKA\Documents\N.Alex-ojukwuCSC101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\NDUKA\Documents\N.Alex-ojukwuCSC101\week-3\practice_10\src\main.rs
