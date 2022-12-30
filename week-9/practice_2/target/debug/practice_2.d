845a7c0787d8f6673bce7a4d963c27f7cade261f
nduka <nduka.alex-ojukwu@pau.edu.ng>
2022-12-30 15:24:36 +0100

    (my eight commit)

---
+++ week-9/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\NDUKA\Documents\N.Alex-ojukwuCSC101\week-9\practice_2\target\debug\practice_2.exe: C:\Users\NDUKA\Documents\N.Alex-ojukwuCSC101\week-9\practice_2\src\main.rs
---
+++ week-9/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\NDUKA\Documents\N.Alex-ojukwuCSC101\week-9\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+C:\Users\NDUKA\Documents\N.Alex-ojukwuCSC101\week-9\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+src\main.rs:
